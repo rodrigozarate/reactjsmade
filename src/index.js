d15122959e59ead227dbf885a4c17d8ec531d8a5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-02-21 13:43:17 -0500

    Add const status

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ class Board extends React.Component {
   }
 
   render() {
-    'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
